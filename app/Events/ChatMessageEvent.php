96a740c1d75704f3d204f8f682b12174431e1866
Tophe <bezedache29@gmail.com>
2021-10-20 22:33:32 +0200

    Mise en place d'un evenement ChatMessageEvent + methode store du ChatController

---
+++ app/Events/ChatMessageEvent.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ChatMessageEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $nickname;
+    public $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(String $nickname, String $message)
+    {
+        $this->nickname = $nickname;
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('chat');
+    }
+
+    // Pour eviter les pb de fqdn
+    public function broadcastAs()
+    {
+        return 'chat-message';
+    }
+}
